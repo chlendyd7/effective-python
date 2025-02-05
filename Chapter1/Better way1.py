40f1d98474837d9c0da107f03ce1a80438b1bd83
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2025-02-05 23:14:45 +0900

    250205

---
+++ Chapter1/Better way1.py
@@ -0,0 +1,9 @@
+# $python --version
+# Python 3.8.0
+
+import sys
+print(sys.version_info) 
+# sys.version_info(major=3, minor=12, micro=8, releaselevel='final', serial=0)
+
+print(sys.version)
+# 3.12.8 (tags/v3.12.8:2dc476b, Dec  3 2024, 19:30:04) [MSC v.1942 64 bit (AMD64)]
