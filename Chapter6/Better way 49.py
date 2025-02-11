7c1cd47aea2b7477a66fffee08e98d2af3f07f2e
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2025-02-11 16:59:16 +0900

    250211

---
+++ Chapter6/Better way 49.py
@@ -0,0 +1,95 @@
+import json
+
+
+class Serializable:
+    def __init__(self, *args):
+        self.args = args
+
+    def serialize(self):
+        return json.dumps({'args': self.args})
+
+
+class Point2D(Serializable):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Point2D({self.x}, {self.y})'
+
+point = Point2D(5, 3)
+print('객체:', point)
+print('직렬화한 값:', point.serialize())
+
+
+class Deserializable(Serializable):
+    @classmethod
+    def deserialize(cls, json_data):
+        params = json.loads(json_data)
+        return cls(*params['args'])
+
+class Deserializable(Serializable):
+    @classmethod
+    def deserialize(cls, json_data):
+        params = json.loads(json_data)
+        return cls(*params['args'])
+
+
+class BetterPoint2D(Deserializable):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'BetterPoint2D({self.x}, {self.y})'
+
+# before = BetterPoint2D(5, 3)
+# print('이전: ', before)
+# data = before.serialize()
+# print('직렬화한 값:', data)
+# after = BetterPoint2D.deserialize(data)
+# print('이후: ', after)
+
+
+class BetterSerializable:
+    def __init__(self, *args):
+        self.args = args
+
+    def serialize(self):
+        return json.dumps({
+            'class': self.__class__.__name__,
+            'args': self.args,
+        })
+
+    def __repr__(self):
+        name = self.__class__.__name__
+        args_str = ', '.join(str(x) for x in self.args)
+        return f'{name}({args_str})'
+
+registry = {}
+
+def register_class(target_class):
+    registry[target_class.__name__] = target_class
+
+def deserialize(data):
+    params = json.loads(data)
+    name = params['class']
+    target_class = registry[name]
+    return target_class(*params['args'])
+
+
+class EvenBetterPoint2D(BetterSerializable):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.x = x
+        self.y = y
+
+register_class(EvenBetterPoint2D)
+before = EvenBetterPoint2D(5, 3)
+print('이전: ', before)
+data = before.serialize()
+print('직렬화한 값:', data)
+after = deserialize(data)
+print('이후: ', after)
